926b6f92e0905e43c432971e0d6b6cb598e1619f
yizhaosan <2316453754@qq.com>
2020-08-11 14:03:39 +0800

    刘宗顺-Login可用，

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Index from "../views/Index.vue";
+import Login from "../views/Login.vue";
+import BusinessList from "../views/BusinessList.vue";
+// ... ...
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Index,
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+    },
+    {
+        path: "/index",
+        name: "Index",
+        component: Index,
+    },
+    {
+        path: "/businessList",
+        name: "Business",
+        component: BusinessList,
+    },
+];
+
+//解决重复路由报异常问题
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch((err) => err);
+};
+
+const router = new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+});
+
+export default router;
---
+++ src/views/BusinessList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Yizhaosan</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BusinessList"
+    }
+</script>
+
+<style scoped>
+
+</style>
